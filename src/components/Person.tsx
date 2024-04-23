5283c0d150a79c372ec17157ef29153e28c3f0c1
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2024-04-23 19:47:06 -0300

    Criando as pastas e organizando o ReactJS

---
+++ src/components/Person.tsx
@@ -0,0 +1,13 @@
+export const Person = () => {
+    return (
+        <>
+            <h1>Bruno Motta</h1>
+            <img 
+                src="" 
+                alt="Bruno Motta" 
+            />
+
+
+        </> 
+    )
+}
---
+++ src/components/Square.tsx
@@ -0,0 +1,9 @@
+export const Square = () => {
+    return (
+        <div className="w-52 h-52 bg bg-orange-700 text-white ">
+            Texto qualquer inspirador
+        </div>
+    )
+}
+
+
