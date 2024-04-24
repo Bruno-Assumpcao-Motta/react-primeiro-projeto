5588d8e783712768e17e23920691af399ad1162b
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2024-04-24 17:49:20 -0300

    Update Person.tsx

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,10 +1,14 @@
 export const Person = () => {
+
+    const name: string = 'Ayrton Senna'
+    const avatar: string = 'https://www.tailorexec.com.br/wp-content/uploads/2024/03/horizon-chase-turbo-ayrton-senna-1024x576.jpg'
+
     return (
         <>
-            <h1>Ayrton Senna</h1>
+            <h1>{name}</h1>
             <img 
-                src="https://www.tailorexec.com.br/wp-content/uploads/2024/03/horizon-chase-turbo-ayrton-senna-1024x576.jpg" 
-                alt="Ayrton Senna" 
+                src={avatar} 
+                alt={name} 
                 className="w-52"
             />
             <ul>
